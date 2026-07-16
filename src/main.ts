d8a3f7f08be29710ee8b5157f869bc17adafc8eb
Lessa Apolinario <lessaapolinario@gmail.com>
2026-07-16 15:34:14 -0300

    Add pinia setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./ui/routes";
 import "./ui/styles/globals.css";
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
 
 app.mount("#app");
