802ab97f771f6fad7e77d9ed9f7bd490477861b6
Lessa Apolinario <lessaapolinario@gmail.com>
2026-07-16 13:58:24 -0300

    Add tailwind class sorting

--- src/ui/pages/HomePage.vue
+++ src/ui/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="text-red-800 bg-red-200 p-4">Home Page</div>
+  <div class="bg-red-200 p-4 text-red-800">Home Page</div>
 </template>
 
 <style scoped></style>
